85449d4d649deb19e410692bb4fd2a2c2ee3172e
Yuval-Zohar <yuzy1@walla.co.il>
2019-09-07 22:01:48 +0300

    Add files via upload

---
+++ mooc2/jScripts/dropDown.js
@@ -0,0 +1,54 @@
+﻿function showData() {
+    var theSelect = chooseName.selectName;
+    var firstP = document.getElementById('firstP');
+    var secondP = document.getElementById('secondP');
+
+    if (theSelect[theSelect.selectedIndex].value == "A") {
+        firstP.innerHTML = ("כל הכבוד");
+        secondP.innerHTML = ("");
+    }
+    else if (theSelect[theSelect.selectedIndex].value == "B") {
+        firstP.innerHTML = ("");
+        secondP.innerHTML = ("נסה שוב");
+    }
+    else if (theSelect[theSelect.selectedIndex].value == "C") {
+        firstP.innerHTML = ("");
+        secondP.innerHTML = ("נסה שוב");
+    }
+    else if (theSelect[theSelect.selectedIndex].value == "D") {
+        firstP.innerHTML = ("");
+        secondP.innerHTML = ("נסה שוב");
+    }
+    else {
+        firstP.innerHTML = ("");
+        secondP.innerHTML = ("");
+    }
+}
+
+
+function showData2() {
+    var theSelect = chooseName2.selectName2;
+    var firstP = document.getElementById('firstP2');
+    var secondP = document.getElementById('secondP2');
+
+    if (theSelect[theSelect.selectedIndex].value == "A") {
+        firstP.innerHTML = ("");
+        secondP.innerHTML = ("נסה שוב");
+    }
+    else if (theSelect[theSelect.selectedIndex].value == "B") {
+        firstP.innerHTML = ("כל הכבוד");
+        secondP.innerHTML = ("");
+    }
+    else if (theSelect[theSelect.selectedIndex].value == "C") {
+        firstP.innerHTML = ("");
+        secondP.innerHTML = ("נסה שוב");
+    }
+    else if (theSelect[theSelect.selectedIndex].value == "D") {
+        firstP.innerHTML = ("");
+        secondP.innerHTML = ("נסה שוב");
+    }
+    else {
+        firstP.innerHTML = ("");
+        secondP.innerHTML = ("");
+    }
+}
